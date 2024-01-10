463aff43eac563a5d270a4cd9c582a7b7845c102
france <frongrider2@gmail.com>
2024-01-11 02:24:51 +0700

    add ip port proxy

--- dbAndEth/proxy/index.js
+++ dbAndEth/proxy/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/', createProxyMiddleware({ 
-  target: 'http://chain.reliefer.online:8545', // target host
+  target: 'http://157.245.145.145:8545', // target host
   changeOrigin: true, // needed for virtual hosted sites
   ws: true, // proxy websockets
 }));
